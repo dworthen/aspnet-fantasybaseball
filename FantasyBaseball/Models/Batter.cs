38b7fb08370925aa54726d56628318b9d35dc20f
dworthen <worthend.derek@gmail.com>
2014-08-09 10:17:10 -0700

    Added models.

---
+++ FantasyBaseball/Models/Batter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FantasyBaseball.Models {
+    public class Batter : Player {
+        public string Bats { get; set; }
+    }
+}
---
+++ FantasyBaseball/Models/Pitcher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FantasyBaseball.Models {
+    public class Pitcher : Player {
+        public string Throws { get; set; }
+    }
+}
